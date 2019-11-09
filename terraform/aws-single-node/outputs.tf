bd5152b841c58f853490d41f9e83cce84a42bc06
Amitabh Prasad <prasadamitabh@gmail.com>
2019-11-09 16:03:17 +0530

    added single node vm

---
+++ terraform/aws-single-node/outputs.tf
@@ -0,0 +1,40 @@
+#####################################################################
+##
+##      Created 10/1/19 by ucdpadmin for cloud aws-brad. for aws-two-node
+##
+#####################################################################
+
+output "camtags_tagsmap" {
+  value = "${module.camtags.tagsmap}"
+}
+
+output "camtags_tagslist" {
+  value = "${module.camtags.tagslist}"
+}
+
+# Web server IP address
+output "web_server_ip_address" {
+  value = "${aws_instance.web-server.public_ip}"
+}
+
+# Web server IP address with user
+output "web_server_ip_address_with_user" {
+  value = "${var.centos-user}@${aws_instance.web-server.public_ip}"
+}
+
+# Web server IP address with user and key
+output "web_server_ip_address_with_user_and_key" {
+  value = { "${var.centos-user}@${aws_instance.web-server.public_ip}" = "${base64encode(tls_private_key.ssh.private_key_pem)}" }
+}
+
+
+# Encoded TLS private key
+output "tls-private-key" {
+  value = "${base64encode(tls_private_key.ssh.private_key_pem)}"
+}
+
+# Centos AMI user
+output "web-server-user" {
+  value = "${var.system_user}"
+}
+
